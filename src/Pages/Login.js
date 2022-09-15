e29ae45ed2d432b7890963128da0d948073b0238
lbenjaminq <benjaminlbq12@gmail.com>
2022-09-15 14:04:34 -0300

    Feat(src): adding NetflixButton and NetflixInput

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core'
+import { makeStyles, Typography } from '@material-ui/core'
 import NetflixLogo from '../Images/NetflixLogo.png'
 import NetflixFondo from '../Images/netflixFondo.png'
+import { NetflixButton, NetflixInput } from '../styled'
 
 const Login = () => {
 
@@ -9,8 +10,20 @@ const Login = () => {
 
   return (
     <div className={classes.root}>
-      <img src={NetflixLogo} className={classes.logo} />
-      {/* <NetflixButton /> */}
+        <img src={NetflixLogo} className={classes.logo} />
+        <NetflixButton className={classes.session}>Iniciar sesión</NetflixButton>
+      <div className={classes.subroot}>
+        <Typography variant='h2'>Películas y series ilimitadas y mucho más</Typography>
+        <Typography variant='h5'>Disfruta donde quieras. Cancela cuando   quieras
+        </Typography>
+        <Typography variant='h6'>¿Quieres ver Netflix ya? Ingresa tu email para crear una cuenta o reiniciar tu membresía de Netflix
+        </Typography>
+        <div className={c}>
+
+          <NetflixButton>GET STARTED</NetflixButton>
+        </div>
+      <div className={classes.background} />
+      </div>
     </div>
   )
 }
@@ -22,10 +35,44 @@ const useStyles = makeStyles((theme)=>({
     backgroundPosition:'center',
     backgroundRepeat:'no-repeat',
     backgroundSize:'cover',
-    objectFit:'contain'
+    objectFit:'contain',
+    position:'relative',
   },
   logo:{
-    width:'100px'
+    width:'100px',
+    zIndex:"100"
+  },
+  logo:{
+    position:"fixed",
+    top:0,
+    left:0,
+    width:"150px",
+    cursor:'pointer',
+    zIndex:"100"
+  },
+  session:{
+    position:"fixed",
+    right:20,
+    top:20,
+    zIndex:"100"
+  },
+  subroot:{
+    color:'white',
+    height:'100vh',
+    display:'flex',
+    flexDirection:'column',
+    justifyContent:'center',
+    alignItems:'center',
+    "& h2,h5,h6":{
+      zIndex:"100",
+    }
+  },
+  background:{
+    position:'absolute',
+    height:'100vh',
+    width:'100%',
+    backgroundColor:'rgba(0,0,0,0.5)',
+    zIndex:"1"
   }
 }))
 
---
+++ src/styled.js
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+import { InputBase } from "@material-ui/core"
+
+// export const NetflixInput = styled(InputBase)`
+// width:100px;
+// color:red;
+// background-color:white,
+// border:2px solid red
+// `
+
+export const NetflixButton = styled.button`
+background-color:red;
+width:150px;
+height:50px;
+border:none;
+color:white;
+font-size:1.2rem;
+
+`
