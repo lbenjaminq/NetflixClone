d184ff58594724700c8acf392c7e77df9a6b2174
lbenjaminq <benjaminlbq12@gmail.com>
2022-09-20 12:34:13 -0300

    Refactor(src): Responsive

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -24,7 +24,7 @@ function CollapsibleExample() {
         setUserActive(profile.email);
       });
     }
-  },[]);
+  }, []);
 
   useEffect(() => {
     window.addEventListener("scroll", hideHeader);
@@ -54,13 +54,37 @@ function CollapsibleExample() {
       <a href="/home" style={{ marginLeft: "2.80%" }}>
         <img src={NetflixLogo} style={{ width: "115px" }} alt="Logo" />
       </a>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Toggle
+        aria-controls="responsive-navbar-nav"
+        style={{ backgroundColor: "#787D81", border: "none" }}
+      />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="me-auto">
+          <div className={classes.container_mobile}>
+            <MdEdit />
+            <a href="/manage" className={classes.mobile}>
+              Manage profiles
+            </a>
+          </div>
+          <div className={classes.container_mobile}>
+            <BsFillPersonFill />
+            <a href="/profile" className={classes.mobile}>
+              Account
+            </a>
+          </div>
+          <div className={classes.container_mobile}>
+            <MdOutlineHelp />
+            <a
+              href="https://help.netflix.com/es"
+              target="blank"
+              className={classes.mobile}
+            >
+              Help Center
+            </a>
+          </div>
+          <hr/>
           <Nav.Link href="#features">
-            <span className={classes.link} style={{ marginLeft: "20px" }}>
-              Home
-            </span>
+            <span className={classes.home}>Home</span>
           </Nav.Link>
           <Nav.Link href="#pricing">
             <span className={classes.link}>TV Shows</span>
@@ -77,86 +101,96 @@ function CollapsibleExample() {
           <Nav.Link href="#pricing">
             <span className={classes.link}>Browse by Languages</span>
           </Nav.Link>
+          <hr/>
+          <a onClick={handleSignUp} className={classes.signOut}>
+            Sign out of Netflix
+          </a>
         </Nav>
-        <Nav style={{ margin: "0px 3%", color: "##e5e5e5" }}>
-          <Nav.Link
-            href="#pricing"
-            style={{ margin: "0px 3%", color: "#e5e5e5", fontSize: "1.3rem" }}
-          >
-            <FaSearch />
-          </Nav.Link>
-          <Nav.Link href="#memes">
-            <span className={classes.link}>Kids</span>
-          </Nav.Link>
-          <Nav.Link
-            href="#pricing"
-            style={{ margin: "0px 3%", color: "#e5e5e5" }}
-          >
-            <MdNotifications />
-          </Nav.Link>
 
-          <NavDropdown
-            id="nav-dropdown-dark-example"
-            title={
-              <img
-                src={NetflixAvatar}
-                alt="User"
-                style={{
-                  borderRadius: "5px",
-                  width: "30px",
-                  height: "30px",
-                }}
-              />
-            }
-            menuVariant="dark"
-            align="end"
-          >
-            <NavDropdown.Item href="/profile">
-              <div>
+        <Nav style={{ margin: "0px 3%", color: "##e5e5e5" }}>
+          <div className={classes.responsive}>
+            <Nav.Link
+              href="#pricing"
+              style={{ margin: "0px 3%", color: "#e5e5e5", fontSize: "1.3rem" }}
+            >
+              <FaSearch />
+            </Nav.Link>
+            <Nav.Link href="#memes">
+              <span className={classes.link}>Kids</span>
+            </Nav.Link>
+            <Nav.Link
+              href="#pricing"
+              style={{ margin: "0px 3%", color: "#e5e5e5", fontSize: "1.3rem" }}
+            >
+              <MdNotifications />
+            </Nav.Link>
+          </div>
+
+          <div className={classes.hidden}>
+            <NavDropdown
+              id="nav-dropdown-dark-example"
+              title={
                 <img
-                alt="Avatar"
                   src={NetflixAvatar}
+                  alt="User"
                   style={{
                     borderRadius: "5px",
-                    width: "24px",
-                    height: "24px",
-                    marginRight: "5%",
+                    width: "30px",
+                    height: "30px",
                   }}
                 />
-                <span>{userActive}</span>
-              </div>
-            </NavDropdown.Item>
-            <NavDropdown.Item href="/manage">
-              <div>
-                <MdEdit
-                  style={{ width: "20px", height: "20px", marginRight: "4%" }}
-                />
-                <span className={classes.links}>Manage profiles</span>
-              </div>
-            </NavDropdown.Item>
-            <NavDropdown.Item href="/profile">
-              <div>
-                <BsFillPersonFill
-                  style={{ width: "20px", height: "20px", marginRight: "4%" }}
-                />
-                <span className={classes.links}>Account</span>
-              </div>
-            </NavDropdown.Item>
-            <NavDropdown.Item href="https://help.netflix.com/es" target="blank">
-              <div>
-                <MdOutlineHelp
-                  style={{ width: "20px", height: "20px", marginRight: "4%" }}
-                />
-                <span className={classes.links}>Help Center</span>
-              </div>
-            </NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item onClick={handleSignUp}> 
-              <span className={classes.links}>
-                Sign out of Netflix
-              </span>
-            </NavDropdown.Item>
-          </NavDropdown>
+              }
+              menuVariant="dark"
+              align="end"
+            >
+              <NavDropdown.Item href="/profile">
+                <div>
+                  <img
+                    alt="Avatar"
+                    src={NetflixAvatar}
+                    style={{
+                      borderRadius: "5px",
+                      width: "24px",
+                      height: "24px",
+                      marginRight: "5%",
+                    }}
+                  />
+                  <span>{userActive}</span>
+                </div>
+              </NavDropdown.Item>
+              <NavDropdown.Item href="/manage">
+                <div>
+                  <MdEdit
+                    style={{ width: "20px", height: "20px", marginRight: "4%" }}
+                  />
+                  <span className={classes.links}>Manage profiles</span>
+                </div>
+              </NavDropdown.Item>
+              <NavDropdown.Item href="/profile">
+                <div>
+                  <BsFillPersonFill
+                    style={{ width: "20px", height: "20px", marginRight: "4%" }}
+                  />
+                  <span className={classes.links}>Account</span>
+                </div>
+              </NavDropdown.Item>
+              <NavDropdown.Item
+                href="https://help.netflix.com/es"
+                target="blank"
+              >
+                <div>
+                  <MdOutlineHelp
+                    style={{ width: "20px", height: "20px", marginRight: "4%" }}
+                  />
+                  <span className={classes.links}>Help Center</span>
+                </div>
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item onClick={handleSignUp}>
+                <span className={classes.links}>Sign out of Netflix</span>
+              </NavDropdown.Item>
+            </NavDropdown>
+          </div>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
